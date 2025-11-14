0416697a4191ec1e99abb23e2c4dd6608be3300e
silcaclei <silviacachonleiva@gmail.com>
2025-11-14 17:01:30 +0100

    Ajustando css

--- src/pages/DetalleInterprete.jsx
+++ src/pages/DetalleInterprete.jsx
@@ -2,40 +2,42 @@ import { Link, useParams } from "react-router-dom";
 import peliculas from "../data/peliculas";
 
 function DetalleInterprete() {
-  const { id } = useParams();
+    const { id } = useParams();
 
-  const actores = peliculas.flatMap(pelicula => pelicula.actores); 
-  const interprete = actores[Number(id)]; 
+    const actores = peliculas.flatMap(pelicula => pelicula.actores);
+    const interprete = actores[Number(id)];
 
-  if (!interprete) return <p className="text-center mt-8">Intérprete no encontrado</p>;
+    if (!interprete) return <p className="text-center mt-8">Intérprete no encontrado</p>;
 
-  return (
-  <>
-        <Link to="/interpretes">
-          <button className="px-4 py-2 bg-(--color-primary) text-white text-bold rounded hover:bg-(--color-secondary) hover:text-(--color-primary) transition">
-            Volver
-          </button>
-        </Link>
+    return (
+        <>
 
+            <section className="max-w-5xl mx-auto mt-8 p-4">
+                <nav className="w-full flex justify-end mt-4">
+                    <Link to="/interpretes">
+                        <button className="px-4 py-2 bg-[var(--color-primary)] text-white font-bold rounded hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transition">
+                            Volver
+                        </button>
+                    </Link>
+                </nav>
 
-    <section className="max-w-5xl mx-auto mt-8 p-4">
-      <figure className="flex flex-col md:flex-row gap-6 items-start">
-        <img
-          src={interprete.imagen}
-          alt={`Foto de ${interprete.nombre}`}
-          className="w-full md:w-64 rounded-lg object-cover"
-        />
-        <figcaption className="flex-1">
-          <h2 className="heading-base heading-h3 text-[var(--color-primary)] mb-4">
-            {interprete.nombre}
-          </h2>
-          <p className="text-base-style text-medium">{interprete.biografia}</p>
-        </figcaption>
-      </figure>
-    </section>
+                <figure className="flex flex-col md:flex-row gap-6 items-start">
+                    <img
+                        src={interprete.imagen}
+                        alt={`Foto de ${interprete.nombre}`}
+                        className="w-full md:w-64 rounded-lg object-cover"
+                    />
+                    <figcaption className="flex-1">
+                        <h2 className="heading-base heading-h3 text-[var(--color-primary)] mb-4">
+                            {interprete.nombre}
+                        </h2>
+                        <p className="text-base-style text-medium">{interprete.biografia}</p>
+                    </figcaption>
+                </figure>
+            </section>
 
-    </>
-  );
+        </>
+    );
 }
 
 export default DetalleInterprete;
--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -7,7 +7,7 @@ function MainContent() {
       id="main-content"
       role="main"
       tabIndex="-1"
-      className="min-h-screen bg-linear-to-br from-gray-100 to-gray-200 flex flex-col items-center justify-center p-8"
+      className="min-h-screen bg-linear-to-br from-gray-100 to-gray-200 flex flex-col items-center  p-8"
     >
         <Outlet />
     </main>
